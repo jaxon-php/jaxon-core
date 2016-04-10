49fe09fab8a1dee316898cd582909a6cbe1c6f1f
feuzeu <thierry.feuzeu@gmail.com>
2016-04-09 21:44:09 -0400

    Translated error messages to french.

--- translations/en/errors.php
+++ translations/en/errors.php
@@ -6,10 +6,7 @@ return array(
 	'errors.debug.write-log' => "Xajax was unable to write to the error log file: :file",
 	'errors.debug.message' => "PHP Error Messages: :message",
 	'errors.response.result.invalid' => "An invalid response was returned while processing this request.",
-	'errors.response.data.invalid' => "The xajax response object could not load commands as the data provided was not a valid array.",
-	'errors.response.class.invalid' => "Invalid class (:name) specified for html control; should be %inline, %block or %flow.",
-	'errors.response.control.invalid' => "Invalid control (:class) passed to addChild; should be derived from Xajax\\Response\\Control.",
-	'errors.response.parameter.invalid' => "Invalid parameter passed to xajaxControl::addChildren; should be array of Xajax\\Response\\Control objects",
+	'errors.response.data.invalid' => "The Xajax response object could not load commands as the data provided was not valid.",
 	// A afficher lorsque l'exception DetectUri est lancée.
 	'errors.uri.detect.message' => "Xajax failed to automatically identify your Request URI.",
 	'errors.uri.detect.advice' => "Please set the Request URI explicitly when you instantiate the Xajax object.",
@@ -20,13 +17,13 @@ return array(
 	'errors.mismatch.types' => "Cannot mix response types while processing a single request: :class",
 	'errors.events.invalid' => "Invalid event request received; no event was registered with the name :name.",
 	'errors.functions.invalid' => "Invalid function request received; no request processor found with the name :name.",
-	'errors.functions.invalid-declaration' => "Invalid function declaration for UserFunction.",
+	'errors.functions.invalid-declaration' => "Invalid function declaration.",
 	'errors.objects.invalid' => "Invalid object request received; no object :class or method :method found.",
 	'errors.objects.instance' => "To register a callable object, please provide an instance of the desired class.",
 	'errors.register.method' => "Failed to locate registration method for the following: :args",
 	'errors.register.invalid' => "Attempt to register invalid plugin: :name; " .
 		"should be derived from Xajax\\Plugin\\Request or Xajax\\Plugin\\Response.",
-	'errors.component.load' => "Error: the :name Javascript component could not be included. Perhaps the URL is incorrect?\\nURL: :url",
+	'errors.component.load' => "The :name javascript component could not be included. Perhaps the URL is incorrect?\\nURL: :url",
 	'errors.output.already-sent' => "Output has already been sent to the browser at :location.",
 	'errors.output.advice' => "Please make sure the command \$xajax->processRequest() is placed before this.",
 );
