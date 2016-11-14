eda21a54330e919d200f554f2a04b8af7acc8182
feuzeu <thierry.feuzeu@gmail.com>
2016-11-14 05:25:50 -0500

    A Jaxon call now accepts arrays and objects as parameters.

--- src/Request/Factory.php
+++ src/Request/Factory.php
@@ -59,6 +59,14 @@ class Factory
             {
                 $xRequest->addParameter(Jaxon::BOOL_VALUE, $xParam);
             }
+            else if(is_array($xArgument))
+            {
+                $this->addParameter(Jaxon::JS_VALUE, json_encode($xParam, JSON_FORCE_OBJECT));
+            }
+            else if(is_object($xArgument))
+            {
+                $this->addParameter(Jaxon::JS_VALUE, json_encode($xParam));
+            }
         }
         return $xRequest;
     }
