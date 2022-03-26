37103906b83d1e021deb290c796603438ee9b84f
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2022-03-26 03:44:29 +0100

    Added config manager tests.

---
+++ tests/config/depth.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'core' => [
+        'one' => [
+            'two' => [
+                'three' => [
+                    'four' => [
+                        'five' => [
+                            'six' => [
+                                'seven' => [
+                                    'eight' => [
+                                        'nine' => [
+                                            'ten' => [
+                                                'param' => 'Value',
+                                            ],
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
