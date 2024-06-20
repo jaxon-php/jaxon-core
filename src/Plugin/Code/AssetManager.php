3def7df7296331483010cf12193a0ec299c72b8b
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2024-06-20 02:02:45 +0200

    Bugfix #128: use the js lib with the fix.

--- src/Plugin/Code/AssetManager.php
+++ src/Plugin/Code/AssetManager.php
@@ -35,7 +35,7 @@ class AssetManager
      *
      * @var string
      */
-    const JS_LIB_URL = 'https://cdn.jsdelivr.net/gh/jaxon-php/jaxon-js@4.0/dist';
+    const JS_LIB_URL = 'https://cdn.jsdelivr.net/gh/jaxon-php/jaxon-js@4.0.2/dist';
 
     /**
      * The constructor
