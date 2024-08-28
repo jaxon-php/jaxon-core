56c299e0bc8a1fa2f32497b24146cacacb04edf7
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2024-08-28 03:11:28 +0200

    Fix in js class generation, when a class and a dir have the same name.

--- templates/callables/object.js.php
+++ templates/callables/object.js.php
@@ -1,6 +1,7 @@
-<?php echo $this->sPrefix ?><?php echo $this->sClass ?> = {};
+<?php $sJsClass = $this->sPrefix . $this->sClass ?>
+if(<?php echo $sJsClass ?> === undefined) <?php echo $sJsClass ?> = {};
 <?php foreach($this->aMethods as $aMethod): ?>
-<?php echo $this->sPrefix ?><?php echo $this->sClass ?>.<?php echo $aMethod['name'] ?> = function() {
+<?php echo $sJsClass ?>.<?php echo $aMethod['name'] ?> = function() {
     return jaxon.request({ jxncls: '<?php echo $this->sClass ?>', jxnmthd: '<?php
             echo $aMethod['name'] ?>' }, { parameters: arguments<?php
             foreach($aMethod['config'] as $sKey => $sValue): ?>, <?php
