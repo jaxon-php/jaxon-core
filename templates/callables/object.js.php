f3dffc85ac31391046d4573109eaa233318eb4ac
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2024-08-31 10:59:58 +0200

    Fixed js class generation.

--- templates/callables/object.js.php
+++ templates/callables/object.js.php
@@ -1,10 +1,19 @@
 <?php $sJsClass = $this->sPrefix . $this->sClass ?>
-if(<?php echo $sJsClass ?> === undefined) <?php echo $sJsClass ?> = {};
+<?php
+// An exported PHP class can have the same name as a subdir in the same parent dir.
+// So, we must make sure that embedded js objects do not already exist, before we create them.
+if(strpos($sJsClass, '.') !== false): ?>
+if(<?php echo $sJsClass ?> === undefined) {
+    <?php echo $sJsClass ?> = {};
+}
+<?php else: ?>
+<?php echo $sJsClass ?> = {};
+<?php endif ?>
 <?php foreach($this->aMethods as $aMethod): ?>
 <?php echo $sJsClass ?>.<?php echo $aMethod['name'] ?> = function() {
     return jaxon.request({ jxncls: '<?php echo $this->sClass ?>', jxnmthd: '<?php
-            echo $aMethod['name'] ?>' }, { parameters: arguments<?php
-            foreach($aMethod['config'] as $sKey => $sValue): ?>, <?php
-            echo $sKey ?>: <?php echo $sValue ?><?php endforeach ?> });
+        echo $aMethod['name'] ?>' }, { parameters: arguments<?php
+        foreach($aMethod['config'] as $sKey => $sValue): ?>, <?php
+        echo $sKey ?>: <?php echo $sValue ?><?php endforeach ?> });
 };
 <?php endforeach;
