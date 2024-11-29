2c501a308eb9b0401334627a2bd136fb6b67b928
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2024-11-29 04:18:02 +0100

    Bugfix #141: updated the js lib to 4.0.8.

--- src/Plugin/Code/AssetManager.php
+++ src/Plugin/Code/AssetManager.php
@@ -35,7 +35,7 @@ class AssetManager
      *
      * @var string
      */
-    const JS_LIB_URL = 'https://cdn.jsdelivr.net/gh/jaxon-php/jaxon-js@4.0.7/dist';
+    const JS_LIB_URL = 'https://cdn.jsdelivr.net/gh/jaxon-php/jaxon-js@4.0.8/dist';
 
     /**
      * The constructor
