10f778cb9bd92160b785ace17d451a244dfc0c7d
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2024-12-14 01:56:56 +0100

    Fix.

--- src/Plugin/Request/CallableClass/CallableObject.php
+++ src/Plugin/Request/CallableClass/CallableObject.php
@@ -27,7 +27,7 @@
 namespace Jaxon\Plugin\Request\CallableClass;
 
 use Jaxon\App\AbstractCallable;
-use Jaxon\App\PaginationComponent;
+use Jaxon\App\Ajax\Pagination;
 use Jaxon\Di\ClassContainer;
 use Jaxon\Di\Container;
 use Jaxon\Exception\SetupException;
@@ -181,7 +181,7 @@ class CallableObject
     public function excluded(): bool
     {
         return $this->xOptions->excluded() || $this->xReflectionClass->isAbstract() ||
-            $this->xReflectionClass->isSubclassOf(PaginationComponent::class);
+            $this->xReflectionClass->isSubclassOf(Pagination::class);
     }
 
     /**
