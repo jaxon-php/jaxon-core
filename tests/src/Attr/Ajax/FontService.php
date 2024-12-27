e18e4ddc7031543dcf2c469020c6db59f6517242
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2024-12-27 15:30:26 +0100

    Movd attributes tests to the jaxon-attributes package.

--- tests/src/Attr/Ajax/FontService.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Jaxon\Tests\App\Attr\Ajax;
-
-class FontService
-{}
--- tests/src/Attr/CallableClass.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Jaxon\Tests\App\Attr;
-
-class CallableClass extends \Jaxon\App\CallableClass
-{
-}
