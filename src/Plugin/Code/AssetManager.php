a8217fe9242926de4c567e51b8db322affdd1540
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2024-12-29 00:44:45 +0100

    Bump the js package version number.

--- src/Plugin/Code/AssetManager.php
+++ src/Plugin/Code/AssetManager.php
@@ -35,7 +35,7 @@ class AssetManager
      *
      * @var string
      */
-    const JS_LIB_URL = 'https://cdn.jsdelivr.net/gh/jaxon-php/jaxon-js@5.0.0-beta.8/dist';
+    const JS_LIB_URL = 'https://cdn.jsdelivr.net/gh/jaxon-php/jaxon-js@5.0.0-beta.9/dist';
 
     /**
      * The constructor
--- tests/src/js/plugin.js
+++ tests/src/js/plugin.js
@@ -3,8 +3,8 @@
     cursor: pointer;
   }
 </style>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/jaxon-php/jaxon-js@5.0.0-beta.6/dist/libs/chibi/chibi.js"  charset="UTF-8"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/jaxon-php/jaxon-js@5.0.0-beta.6/dist/jaxon.core.js"  charset="UTF-8"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/jaxon-php/jaxon-js@5.0.0-beta.9/dist/libs/chibi/chibi.js"  charset="UTF-8"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/jaxon-php/jaxon-js@5.0.0-beta.9/dist/jaxon.core.js"  charset="UTF-8"></script>
 
 <script type="text/javascript"  charset="UTF-8">
 /* <![CDATA[ */
