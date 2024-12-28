016a76ea2dcb96fc8f1b37da8acbb7c9ed0a3e20
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2024-12-28 23:58:39 +0100

    Bump the js package version number.

--- src/Plugin/Code/AssetManager.php
+++ src/Plugin/Code/AssetManager.php
@@ -35,7 +35,7 @@ class AssetManager
      *
      * @var string
      */
-    const JS_LIB_URL = 'https://cdn.jsdelivr.net/gh/jaxon-php/jaxon-js@5.0.0-beta.6/dist';
+    const JS_LIB_URL = 'https://cdn.jsdelivr.net/gh/jaxon-php/jaxon-js@5.0.0-beta.8/dist';
 
     /**
      * The constructor
