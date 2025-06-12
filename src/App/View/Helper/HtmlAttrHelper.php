3a63f074e2f151be647a23d862b985b624cb77e9
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2025-06-12 07:30:55 +0200

    New event setters in the custom attr helper.

--- src/App/View/Helper/HtmlAttrHelper.php
+++ src/App/View/Helper/HtmlAttrHelper.php
@@ -20,8 +20,11 @@ use Jaxon\Di\ComponentContainer;
 use Jaxon\Script\JsExpr;
 use Jaxon\Script\Call\JxnCall;
 
+use function array_reduce;
+use function count;
 use function htmlentities;
 use function is_a;
+use function is_string;
 use function Jaxon\rq;
 use function json_encode;
 use function trim;
@@ -140,5 +143,62 @@ class HtmlAttrHelper
     public function click(JsExpr $xJsExpr): string
     {
         return $this->on('click', $xJsExpr);
+    }
+
+    /**
+     * @param array $aHandler
+     *
+     * @return bool
+     */
+    private function entryIsValid(array $aHandler): bool
+    {
+        return count($aHandler) === 3 &&
+            isset($aHandler[0]) && isset($aHandler[1]) && isset($aHandler[2]) &&
+            is_string($aHandler[0]) && is_string($aHandler[1]) &&
+            is_a($aHandler[2], JsExpr::class);
+    }
+
+    /**
+     * @param array $aHandler
+     * @param EventAttr|null $xAttr
+     *
+     * @return EventAttr|null
+     */
+    private function setEventHandler(array $aHandler, ?EventAttr $xAttr = null): EventAttr|null
+    {
+        if(!$this->entryIsValid($aHandler))
+        {
+            return $xAttr;
+        }
+        // The array content is valid.
+        [$sSelector, $sEvent, $xJsExpr] = $aHandler;
+        return !$xAttr ?
+            $this->select($sSelector)->on($sEvent, $xJsExpr) :
+            $xAttr->select($sSelector)->on($sEvent, $xJsExpr);
+    }
+
+    /**
+     * Set an event handler
+     *
+     * @param array $aHandler
+     *
+     * @return string
+     */
+    public function event(array $aHandler): string
+    {
+        return $this->setEventHandler($aHandler)?->__toString() ?? '';
+    }
+
+    /**
+     * Set event handlers
+     *
+     * @param array $aHandlers
+     *
+     * @return string
+     */
+    public function events(array $aHandlers): string
+    {
+        return array_reduce($aHandlers, fn(EventAttr|null $xAttr, array $aHandler)
+            => $this->setEventHandler($aHandler, $xAttr), null)?->__toString() ?? '';
     }
 }
