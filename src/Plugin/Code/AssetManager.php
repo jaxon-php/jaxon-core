8d6cfbb266cdf142156d98b4de18e61c20317862
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2025-09-28 22:50:07 +0200

    Update the js library version.

--- src/Plugin/Code/AssetManager.php
+++ src/Plugin/Code/AssetManager.php
@@ -33,7 +33,7 @@ class AssetManager
      *
      * @var string
      */
-    const JS_LIB_URL = 'https://cdn.jsdelivr.net/gh/jaxon-php/jaxon-js@5.0.7/dist';
+    const JS_LIB_URL = 'https://cdn.jsdelivr.net/gh/jaxon-php/jaxon-js@5.0.8/dist';
 
     /**
      * The constructor
