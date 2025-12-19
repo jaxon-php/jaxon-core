8d2778002e0be28430458b2fb534090bdaca913c
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2025-12-19 18:12:44 +0100

    Update the js library version.

--- tests/src/js/lib.js
+++ tests/src/js/lib.js
@@ -4,8 +4,8 @@
   }
 </style>
 
-<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/jaxon-php/jaxon-js@5.0.12/dist/libs/chibi/chibi.js"  charset="UTF-8"></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/jaxon-php/jaxon-js@5.0.12/dist/jaxon.core.js"  charset="UTF-8"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/jaxon-php/jaxon-js@5.1.0./dist/libs/chibi/chibi.js"  charset="UTF-8"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/jaxon-php/jaxon-js@5.1.0./dist/jaxon.core.js"  charset="UTF-8"></script>
 
 <script type="text/javascript"  charset="UTF-8">
 /* <![CDATA[ */
